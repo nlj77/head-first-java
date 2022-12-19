94a35cf685e353ace3af566e451be77b43d366c2
Nick Jones <nick.jones@siliconmtn.com>
2022-12-19 14:09:38 -0700

    Creates battleship game, and gamehelper class

--- HFJ/src/simplebattleship/SimpleDotCom.java
+++ HFJ/src/simplebattleship/SimpleDotCom.java
@@ -8,19 +8,13 @@ public class SimpleDotCom {
 		locationCells = locs;
 	}
 
-		public String checkYourself (String stringGuess) {
-			
+	public String checkYourself (String stringGuess) {
 			int guess = Integer.parseInt(stringGuess);
-			
 			String result = "miss";
-			
 			for (int cell : locationCells) {
 				  if (guess == cell) {
-					
-					result = "hit";
-							
+					result = "hit";	
 					numOfHits++;
-					
 					break;
 				}
 			}
